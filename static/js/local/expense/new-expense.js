591b9c2c54552198acd18ddccf508ae9ad0d8ebf
Oliver Sandoval <sandovalot@hotmail.com>
2022-02-16 10:29:33 -0500

    enlazado con backend Buscar Proveedor

--- static/js/local/expense/new-expense.js
+++ static/js/local/expense/new-expense.js
@@ -1,5 +1,6 @@
 function searchSupplier(){
-    let ruc = document.getElementById('id_ruc').value;
+    let error = false;
+    let ruc = document.getElementById('ruc_or_business_name').value;
 
     if(ruc === ''){
         alert('Debe ingresar un RUC.')
@@ -9,8 +10,8 @@ function searchSupplier(){
 
         let new_supplier = document.getElementById('id_new_supplier');
         new_supplier.classList.remove('hide');
-        /*
-        fetch(base_API + '',{
+        
+        fetch(base_API + 'expense/expense/search_supplier/?ruc_or_business_name='+ruc,{
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -19,32 +20,119 @@ function searchSupplier(){
             }
         })
         .then(function(response){
+            if(response.status == 400)
+                error = true;
             return response.json();
         })
         .then(function(data){
-            let found = document.getElementById('supplier_found');
-            found.style.display = block;
-            let select;
-            if(id == 0){
-                select = document.getElementById('category_product');
-            }else{
-                select = document.getElementById('id_category_product');
-            }
-            
-            for(let i = 0; i < data.length; i++){
-                let option = document.createElement('option');
-                if(data[i].id == id){
-                    option.setAttribute('selected', 'selected');
-                }           
-                option.value = data[i].id;
-                option.text = data[i].description;
-                select.add(option);
-            }
+            console.log(data);
+            if(error)
+                alert(data.mensaje);
         })
         .catch(function(error){
             console.log("RESPUESTA EN CATCH");
             console.log(error);
-        });*/
+        });
     }
 
 }
+
+function loadPaymentType(){
+    fetch(base_API + 'products/category-products/get_categories/',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let select;
+        select = document.getElementById('payment_type');
+        
+        for(let i = 0; i < data.length; i++){
+            let option = document.createElement('option');
+            if(data[i].id == id){
+                option.setAttribute('selected', 'selected');
+            }           
+            option.value = data[i].id;
+            option.text = data[i].name;
+            select.add(option);
+        }
+    })
+    .catch(function(error){
+        console.log("RESPUESTA EN CATCH");
+        console.log(error);
+    });
+}
+
+function loadVoucherType(){
+    fetch(base_API + 'products/category-products/get_categories/',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let select;
+        select = document.getElementById('voucher');
+        
+        for(let i = 0; i < data.length; i++){
+            let option = document.createElement('option');
+            if(data[i].id == id){
+                option.setAttribute('selected', 'selected');
+            }           
+            option.value = data[i].id;
+            option.text = data[i].name;
+            select.add(option);
+        }
+    })
+    .catch(function(error){
+        console.log("RESPUESTA EN CATCH");
+        console.log(error);
+    });
+}
+
+function loadProducts(){
+    fetch(base_API + 'products/products/get_products/',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let select;
+        select = document.getElementById('product');
+        
+        for(let i = 0; i < data.length; i++){
+            let option = document.createElement('option');
+            if(data[i].id == id){
+                option.setAttribute('selected', 'selected');
+            }           
+            option.value = data[i].id;
+            option.text = data[i].name;
+            select.add(option);
+        }
+    })
+    .catch(function(error){
+        console.log("RESPUESTA EN CATCH");
+        console.log(error);
+    });
+}
+
+loadVoucherType();
+loadPaymentType();
+loadProducts();
